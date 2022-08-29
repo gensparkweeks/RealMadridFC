94ff2154f594fe3cf4b824f9f04a932e68bfb694
Minardo Toirac Isla <gensparkweeks@gmail.com>
2022-08-29 18:46:33 -0400

    first commit

---
+++ rmback/src/main/java/com/rmback/RmbackApplication.java
@@ -0,0 +1,13 @@
+package com.rmback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmbackApplication.class, args);
+	}
+
+}
---
+++ rmback/src/test/java/com/rmback/RmbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
